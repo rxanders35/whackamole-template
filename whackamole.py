d48e8fd28c618f4ac99aa7cd2dcf8eb07ab456ac
rxanders35 <reidxanders@outlook.com>
2024-11-18 13:12:30 -0500

    final draft

--- whackamole.py
+++ whackamole.py
@@ -1,25 +1,35 @@
 import pygame
-
+import random
 
 def main():
     try:
         pygame.init()
-        # You can draw the mole with this snippet:
-        # screen.blit(mole_image, mole_image.get_rect(topleft=(x,y)))
-        mole_image = pygame.image.load("mole.png")
         screen = pygame.display.set_mode((640, 512))
+        mole_image = pygame.image.load("mole.png")
+        mole_size = mole_image.get_width()
         clock = pygame.time.Clock()
+        x, y = 0, 0  
         running = True
         while running:
+            screen.fill("light green")
+            for row in range(0, screen.get_height(), mole_size):
+                pygame.draw.line(screen, "black", (0, row), (screen.get_width(), row))
+            for col in range(0, screen.get_width(), mole_size):
+                pygame.draw.line(screen, "black", (col, 0), (col, screen.get_height()))
+            screen.blit(mole_image, mole_image.get_rect(topleft=(x, y)))
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
-            screen.fill("light green")
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    if pygame.Rect(x, y, mole_size, mole_size).collidepoint(event.pos):
+                        x = random.randrange(0, screen.get_width(), mole_size)
+                        y = random.randrange(0, screen.get_height(), mole_size)
+
             pygame.display.flip()
             clock.tick(60)
     finally:
         pygame.quit()
-
 
 if __name__ == "__main__":
     main()
